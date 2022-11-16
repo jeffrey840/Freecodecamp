d2730e8f579d618e7d820534fc29b6ad06df8fdf
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-15 22:32:39 -0600

    added new calc

--- calculator-project/calc.js
+++ calculator-project/calc.js
@@ -1,1 +1,20 @@
 
+function dis(val){
+	document.getElementById("ans").value += val
+}
+
+function se(){
+	document.getElementById("ans").innerHTML="7";
+}
+
+function sol(){
+	let x = document.getElementById("ans").value;
+	let y = eval(x);
+	document.getElementById("ans").value = y;
+}
+
+function clr(){
+	document.getElementById("ans").value = "";
+}
+
+
